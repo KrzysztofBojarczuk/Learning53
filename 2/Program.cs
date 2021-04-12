fa9ae34223c8d0085cbc702f5cf102bfefc1d367
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-12 09:13:59 +0200

    Add project files.

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string again;
+            do
+            {
+                Console.WriteLine("Rzucam kośćmi...");
+                Console.WriteLine("Oto wynik:");
+                Random rand = new Random();
+                rand.Next(1, 6);
+                Console.WriteLine(rand.Next(1, 6));
+                Console.WriteLine(rand.Next(1, 6));
+
+                Console.WriteLine("Chcesz wykonać kolejny rzut? (t = tak)");
+                again = Console.ReadLine();
+
+            } while (again == "t" || again == "T");
+        }
+    }
+}
